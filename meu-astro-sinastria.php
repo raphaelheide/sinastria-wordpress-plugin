149c3c1d0e7f4a4cb6d060fc690fb00ea43e7e4e
Raphael Heide <64502905+raphaelheide@users.noreply.github.com>
2023-03-14 12:43:48 -0300

    Add files via upload

---
+++ meu-astro-sinastria.php
@@ -0,0 +1,86 @@
+<?php
+
+/*
+Plugin Name: Meu Astro Sinastria
+Plugin URI: 
+Description: Sinastria do Amor - Zodíaco
+Version: 1.1
+Author: Raphael Heide
+License: GPL2
+Copyright 2020 Raphael Heide (email : contato@agenciadesigno.com)
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License, version 2, as
+published by the Free Software Foundation.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+*/
+
+
+
+function sinastria_funcao($showlink, $language) {
+	$request = wp_remote_get('https://meuastro.com.br/sinastria-app/index3.php');
+	$response = wp_remote_retrieve_body( $request );
+	echo $response;
+
+}
+
+add_shortcode('sinastria', 'sinastria_funcao');
+
+
+
+class sinastriaWidget extends WP_Widget
+{
+	function __construct()
+	{
+		parent::__construct('sinastriaWidget', __('Sinastria', 'sinastria' ), array ('description' => __( 'Este plugin mostra em seu site a Sisnatria do amor.', 'sinastria')));
+	}
+	function form($instance)
+	{
+		$instance = wp_parse_args( (array) $instance, array( 'title' => 'Sinastria', 'showlink' => '1', 'language' => 'pt' ) );
+		$title = $instance['title'];
+		$showlink = $instance['showlink'];
+		$language = $instance['language'];
+?>
+<p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="vdd_widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo
+$this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></label></p>
+
+
+
+<?php
+	}
+	function update($new_instance, $old_instance)
+	{
+		$instance = $old_instance;
+		$instance['title'] = $new_instance['title'];
+		if($new_instance['showlink'] == '1')
+		{
+			$instance['showlink'] = '1';
+		}
+		else
+		{
+			$instance['showlink'] = '0';
+		}
+		$instance['language'] = $new_instance['language'];
+		return $instance;
+	}
+	function widget($args, $instance)
+	{
+		extract($args, EXTR_SKIP);
+		echo $before_widget;
+		$title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+		if (!empty($title))
+			echo $before_title . $title . $after_title;;
+		$showlink = $instance['showlink'];
+		$language = $instance['language'];
+		echo sinastria_funcao($showlink, $language);
+		echo $after_widget;
+	}
+}
+
+
+add_action( 'widgets_init', create_function('', 'return register_widget("sinastriaWidget");') );
+
+?>
---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== Meu Astro Sinastria ===
+Contributors: Raphael Heide
+Donate link: https://raphaelheide.com
+Tags: horóscopo, amor, compatibilidade, zodiaco
+Requires at least: 2.7.0
+Tested up to: 6.0
+Stable tag: trunk 
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Este plugin gera o resultado da Sinastria do Amor (compatibilidade do amor pelo zodíaco) automaticamente. O plugin atualiza automaticamente os planetas e outros aspectos do mês. Executado por Meu Astro https://meuastro.com.br
+
+== Description ==
+Este plugin gera o resultado da Sinastria do Amor (compatibilidade do amor pelo zodíaco) automaticamente. O plugin atualiza automaticamente os planetas e outros aspectos do mês. Executado por Meu Astro https://meuastro.com.br
+
+
+== Installation ==
+
+
+1. Subir o arquivo para wp-content/plugin
+2. Ativar plugin
+3. Em sua página que deseja mostrar o Plugin, basta escrever [sinastria]
+	Exemplo: crie uma página chamada "Sinastria", "Compatibilidade no Amor" etc. No texto, escreva [sinastria]
+4. Salvar a página.
+ 
+
+== Screenshots ==
+1. Sinastria Meu Astro
+
+== Changelog ==
+
+
+= 1.0 =
+* First stable version.
+
+
+readme updated
